d2c01c3bc74c00030634db894aa6170bf40973b8
Robert Campbell <robertgcampbell95@gmail.com>
2017-04-28 21:42:50 -0700

    Add max_speed attribute to player script
    Freeze player rotation

--- SMBv1/assets/scripts/player.cs
+++ SMBv1/assets/scripts/player.cs
@@ -3,31 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour {
-    public float distToGround;
     public float jump_Force;
     public float forward_force;
+    public float max_speed = 5;
     private Rigidbody2D rb;
     // Use this for initialization
     void Start () {
-        distToGround = GetComponent<BoxCollider2D>().bounds.extents.y;
         rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (isGrounded())
         {
-            if (Input.GetKey(KeyCode.LeftArrow))
+            if (Input.GetKey(KeyCode.LeftArrow) && !atMaxVelocity())
             {
                 rb.AddForce(new Vector2(-1 * forward_force, 0));
             }
 
-            if (Input.GetKey(KeyCode.RightArrow))
+            if (Input.GetKey(KeyCode.RightArrow) && !atMaxVelocity())
             {
                 rb.AddForce(new Vector2(forward_force, 0));
             }
 
-            if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded() == true)
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded() == true)
             {
                 Vector2 forceToAdd = Vector2.up * jump_Force + rb.velocity;
                 Debug.Log("Velocity: " + rb.velocity);
@@ -41,10 +41,23 @@ public class player : MonoBehaviour {
     private bool isGrounded()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, -1f, 0), -Vector2.up);
-        if(hit.distance < 0.3f)
+        if(hit.distance < 0.05f)
         {
             return true;
         }
         return false;
+    }
+
+    private bool atMaxVelocity()
+    {
+        if(Mathf.Abs(rb.velocity.x) < max_speed)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
     }
 }
